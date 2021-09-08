9991448152a4cdd5f32e5322cd68c6670ea8c53b
Gabriel Jones <gjones2023@chadwickschool.org>
2021-09-08 15:23:37 -0700

    feat : Makes heightConversion class
    This change adds all of the print messages and variables

---
+++ HeightConverter/src/heightConversion.java
@@ -0,0 +1,16 @@
+
+public class heightConversion {
+	public static void main(String[] args) {
+	  double conversionFactor = 2.54;
+	  String myName = "Pat Wilson";
+	  int age = 22;
+	  double myHeightInches = 67;
+	  double myHeightCM = conversionFactor * myHeightInches;
+	  
+	  System.out.println(myName);
+	  System.out.println("age: " + age);
+	  System.out.println("height: "+myHeightInches);
+	  System.out.println("height: "+myHeightCM);
+	
+	}
+}
